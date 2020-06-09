da83b2ff78f27ffbd8e39d2a0be0ed1ca227b1a3
Gustavo Rubin <gusrubin@gmail.com>
2020-06-08 22:39:56 -0300

    Inverting GPIO button with led for led up when start up with button pressed

--- src/WaiterCallerFirmware/WaiterCallerFirmware.ino
+++ src/WaiterCallerFirmware/WaiterCallerFirmware.ino
@@ -23,8 +23,8 @@ const char *device_serial_number_file = "/device-serial-number.cfg";
 char deviceSerialNumber[30] = "";
 
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin = 0; // the number of the pushbutton pin
-const int ledPin =  2; // the number of the LED pin
+const int buttonPin = 2; // the number of the pushbutton pin
+const int ledPin =  0; // the number of the LED pin
 
 // variables will change:
 int buttonState = 0; // variable for reading the pushbutton status
